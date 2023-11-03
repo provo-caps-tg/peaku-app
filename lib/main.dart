1e29586287c42f78f2491332d8bbd3e3b51b653e
provo-caps-tg <provocapstg@gmail.com>
2023-11-03 11:00:27 -0600

    CommitmentOfGriffin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
@@ -13,10 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PeakU',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 5, 116, 62)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 115, 0)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello',),
+      home: const MyHomePage(title: 'Hi', ),
       
     );
   }
@@ -53,38 +51,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.white,
               ),
               color: Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(29)),),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Align(
-                  alignment: Alignment.center,
-                  child: Image(
-                    image: AssetImage('assets/images/logo.png'),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: TextButton(
-                          style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
-                            backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const LoginPage())); 
-                          },
-                          child: const Text('Get Started'),
-                      )
-                    ),
-                  ),
-                ),
+              borderRadius: const BorderRadius.all(Radius.circular(29))
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
               ],
             ),
           ),
@@ -141,11 +113,7 @@ class _LoginPageState extends State<LoginPage> {
                   image: AssetImage('assets/images/logoimg.png'),
                   fit: BoxFit.fitWidth,
                 ),
-                Positioned(
-                  top: 245,
-                  left: 43,
-                  child: Text('Email', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
-                ),
+                Text('Email', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                   child: TextField(
