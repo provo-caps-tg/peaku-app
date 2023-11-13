68fc302131e323bab073b3d7145f50b37a55c065
provo-caps-tg <provocapstg@gmail.com>
2023-11-13 12:08:43 -0700

    NoChagneJustNEedToCommit

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -184,6 +185,7 @@ class _LoginPageState extends State<LoginPage> {
                             labelText: 'Enter your password',
                           ),
                         obscureText: true,
+             
                       ),
                       
                     ),
@@ -195,10 +197,12 @@ class _LoginPageState extends State<LoginPage> {
                       height: 50,
                       child:  ElevatedButton(
                         onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
-                              },
+                             
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
+                              
+                        },
                         style: ButtonStyle(
                                   foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
                                   backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
@@ -219,42 +223,7 @@ class _LoginPageState extends State<LoginPage> {
 
 
 
-//Form Validation Class starts here
-class LoginForm extends StatefulWidget {
-  const LoginForm({super.key});
 
-  @override
-  LoginFormState createState() {
-    return LoginFormState();
-  }
-}
-
-class LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-  @override
-  
-  Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          // Add TextFormFields and ElevatedButton here.
-          TextFormField
-          (
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please eneter some text';
-              } 
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-}
-//Form Validation class ends here
 
 class DailyQuestionPage extends StatefulWidget {
   const DailyQuestionPage({Key? key}) : super(key: key);
