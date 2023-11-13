2d91c6b43fabf1e09968c766ebb004d174ce1a03
provo-caps-tg <provocapstg@gmail.com>
2023-11-13 11:55:14 -0700

    Better Keyboad

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -113,10 +112,12 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
@@ -144,84 +145,74 @@ class _LoginPageState extends State<LoginPage> {
               color: Colors.white,
               borderRadius: const BorderRadius.all(Radius.circular(29))
             ),
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              
-              
-              //Start of children
-              children: <Widget>[ 
-                
-                const Image(
-                  fit: BoxFit.scaleDown,
-                  image: AssetImage('assets/images/logo.png'),
-                ),
-               
-                const Align(
-                  alignment: Alignment(-.95,1),
-                  child: Text('Email', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
-                ),
-                const Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
-                    
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Enter your username',
-                        ),
-                    )  
+            child: Padding(
+              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: <Widget>[ 
+                  const Image(
+                    fit: BoxFit.scaleDown,
+                    image: AssetImage('assets/images/logo.png'),
                   ),
-                ),
-                const Align(
-                  alignment: Alignment(-.95,1),
-                  child: Text('Password', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
-                ),
-                const Expanded(child: 
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: UnderlineInputBorder(),
-                        labelText: 'Enter your password',
+                  const Align(
+                    alignment: Alignment(-.95,1),
+                    child: Text('Email', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
+                  ),
+                  const Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                      
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your username',
+                          ),
+                      )  
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment(-.95,1),
+                    child: Text('Password', style: TextStyle(fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
+                  ),
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your password',
+                          ),
+                        obscureText: true,
                       ),
-                    obscureText: true,
+                      
+                    ),
                   ),
-                  
-                ),
-                ),
-                Padding(
-                padding: const EdgeInsets.all(20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child:  ElevatedButton(
-                    onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
-                          },
-                    style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
-                              backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
-                            ),
-                    child: const Text("Login"),
-                    )
-                  
-                ),
-                )
-                
-                
-                
-            ],
-              
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child:  ElevatedButton(
+                        onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
+                              },
+                        style: ButtonStyle(
+                                  foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
+                                  backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
+                                ),
+                        child: const Text("Login"),
+                      )
+                    ),
+                  )
+                ],
+              ),
             ),
-            
           ),
-          
         ),
-        
-      ),
+      )
     );
   }
 }
@@ -244,7 +235,6 @@ class _DailyQuestionPage extends State<DailyQuestionPage> {
 
       ),
       body: Container(
-        //background
         width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -253,10 +243,8 @@ class _DailyQuestionPage extends State<DailyQuestionPage> {
           ),
         ),
         child: Padding(
-          //White exterior box
           padding: const EdgeInsets.all(20),
           child: Container(
-            
             decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.white,
@@ -269,14 +257,10 @@ class _DailyQuestionPage extends State<DailyQuestionPage> {
               //Start of children
               children: <Widget>[ 
               
-            ],
-              
+              ],
             ),
-            
           ),
-          
         ),
-        
       ),
     );
   }
