2733075e29c26e569fb546f9cc11b99e89062cd4
provo-caps-tg <provocapstg@gmail.com>
2023-11-17 10:30:33 -0700

    code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api
 void main() {runApp(const MyApp());}
 Color orange = const Color.fromARGB(255, 232, 105, 0);
 Color blue = const Color.fromARGB(255, 15, 49, 86);
@@ -15,7 +16,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
-      
     );
   }
 }
@@ -148,11 +148,9 @@ final passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: orange, foregroundColor: Colors.white, toolbarHeight: 3,
-
       ),
       body: Container(
         //background
@@ -166,136 +164,136 @@ final passController = TextEditingController();
         ),
         child: Stack(
           children: [
-        Padding(
-          //White exterior box
-          padding: const EdgeInsets.all(20),
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.white,
-              ),
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(29))
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                children: <Widget>[ 
-                  const Image(
-                    fit: BoxFit.scaleDown,
-                    image: AssetImage('assets/images/logo.png'),
+            Padding(
+              //White exterior box
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
                   ),
-                  Form(key: emailKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                       const Align(
-                          alignment: Alignment.bottomLeft,
-                            child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
-                            child:  Text('Email', style: TextStyle( fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
-                              child: TextFormField(
-                                decoration: const InputDecoration(labelText: 'Enter your email here'),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter some text.';
-                                  }
-                                  return null;
-                                },
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(29))
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    children: <Widget>[ 
+                      const Image(
+                        fit: BoxFit.scaleDown,
+                        image: AssetImage('assets/images/logo.png'),
+                      ),
+                      Form(key: emailKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Align(
+                              alignment: Alignment.bottomLeft,
+                                child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                                child:  Text('Email', style: TextStyle( fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
                               ),
                             ),
-                         ),
-                        ],
-                      ),
-                    ),
-                  Form(key: passKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                       const Align(
-                          alignment: Alignment.bottomLeft,
-                            child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
-                            child:  Text('Password', style: TextStyle( fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
-                              child: TextFormField(
-                                obscureText: true,
-                                enableSuggestions: false,
-                                autocorrect: false,
-                                decoration: const InputDecoration(labelText: 'Enter your password here'),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Please enter some text.';
-                                  }
-                                  return null;
-                                },
-                              ),
-                            ),
-                         ),
-                        ],
-                      ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child:  ElevatedButton(
-                        onPressed: () {
-                          if ((emailKey.currentState!.validate()) && (passKey.currentState!.validate())){
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
-                          }
-                        },
-                        style: ButtonStyle(
-                                  foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
-                                  backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                                child: TextFormField(
+                                  decoration: const InputDecoration(labelText: 'Enter your email here'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter some text.';
+                                    }
+                                    return null;
+                                  },
                                 ),
-                        child: const Text("Login"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Form(key: passKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                          const Align(
+                              alignment: Alignment.bottomLeft,
+                                child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                                child:  Text('Password', style: TextStyle( fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 16.0),
+                                child: TextFormField(
+                                  obscureText: true,
+                                  enableSuggestions: false,
+                                  autocorrect: false,
+                                  decoration: const InputDecoration(labelText: 'Enter your password here'),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter some text.';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child:  ElevatedButton(
+                            onPressed: () {
+                              if ((emailKey.currentState!.validate()) && (passKey.currentState!.validate())){
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => const DailyQuestionPage())); 
+                              }
+                            },
+                            style: ButtonStyle(
+                              foregroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 255, 255, 255)),
+                              backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 232, 105, 0)),
+                            ),
+                            child: const Text("Login"),
+                          )
+                        ),
                       )
-                    ),
-                  )
-                ],
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8, top: 8),
-          child: Container(
-            decoration: BoxDecoration(
-              color: orange,
-              borderRadius: BorderRadius.circular(13),
-            ),
-            child: IconTheme(
-              data: const IconThemeData(
-              color: Colors.white),
-              child: IconButton(
-                onPressed:() {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MyHomePage()), );
-                    }, 
-                icon: const Icon(Icons.arrow_back)
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: orange,
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                child: IconTheme(
+                  data: const IconThemeData(
+                  color: Colors.white),
+                  child: IconButton(
+                    onPressed:() {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MyHomePage()), );
+                        }, 
+                    icon: const Icon(Icons.arrow_back)
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
           ],
         ),
       )
@@ -305,13 +303,10 @@ final passController = TextEditingController();
 
 
 
-//Daily Question Page
-
 class DailyQuestionPage extends StatefulWidget {
   const DailyQuestionPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _DailyQuestionPage createState() => _DailyQuestionPage();
 }
 
@@ -334,7 +329,7 @@ class _DailyQuestionPage extends State<DailyQuestionPage> {
         ),
         child: Stack(
           children: [
-        Padding(
+            Padding(
               padding: const EdgeInsets.all(20),
               child: Container(
                 width: double.infinity,
@@ -346,68 +341,67 @@ class _DailyQuestionPage extends State<DailyQuestionPage> {
                   color: Colors.white,
                   borderRadius: const BorderRadius.all(Radius.circular(29))
                 ),
-                            child: Padding(
-              padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  //Start of children
-                  children: <Widget>[ 
-                    const Image(
-                        fit: BoxFit.scaleDown,
-                        image: AssetImage('assets/images/logo.png'),
-                      ),
-                    const Text(
-                      'Daily Question',
-                      style: TextStyle(height: 0,fontSize: 40, fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric( vertical: 40.0, horizontal: 16,),
-                      child: Text(
-                        'Are you the same as you were 2 years ago?  5 years ago?  What has changed, if anything?',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 0,fontSize: 20,  fontFamily: 'Barlow', color: Color.fromRGBO(83, 83, 83, 1)),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Answer question here'
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      //Start of children
+                      children: <Widget>[ 
+                        const Image(
+                          fit: BoxFit.scaleDown,
+                          image: AssetImage('assets/images/logo.png'),
                         ),
-                      ),
+                        const Text(
+                          'Daily Question',
+                          style: TextStyle(height: 0,fontSize: 40, fontWeight: FontWeight.bold, fontFamily: 'Barlow', color: Color.fromRGBO(15, 49, 86, 1)),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric( vertical: 40.0, horizontal: 16,),
+                          child: Text(
+                            'Are you the same as you were 2 years ago?  5 years ago?  What has changed, if anything?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(height: 0,fontSize: 20,  fontFamily: 'Barlow', color: Color.fromRGBO(83, 83, 83, 1)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Answer question here'
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
+                ),
+              ),
+            ),   
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: orange,
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                child: IconTheme(
+                  data: const IconThemeData(
+                  color: Colors.white),
+                  child: IconButton(
+                    onPressed:() {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginPage()), );
+                        }, 
+                    icon: const Icon(Icons.arrow_back)
+                  ),
                 ),
               ),
             ),
-              ),
-        ),
-              
-        Padding(
-          padding: const EdgeInsets.only(left: 8, top: 8),
-          child: Container(
-            decoration: BoxDecoration(
-              color: orange,
-              borderRadius: BorderRadius.circular(13),
-            ),
-            child: IconTheme(
-              data: const IconThemeData(
-              color: Colors.white),
-              child: IconButton(
-                onPressed:() {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()), );
-                    }, 
-                icon: const Icon(Icons.arrow_back)
-              ),
-            ),
-          ),
-        ),
           ],
-      ),
+        ),
       ),
     );
   }
@@ -417,7 +411,6 @@ class AboutUs extends StatefulWidget {
   const AboutUs({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _AboutUs createState() => _AboutUs();
 }
 
