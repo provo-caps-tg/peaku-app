aab1b50371370f77c955bc0e0de4215368c18ba5
provo-caps-tg <provocapstg@gmail.com>
2023-11-21 12:29:10 -0700

    same page?

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:peaku/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PeakUApp());
 
   });
 }
