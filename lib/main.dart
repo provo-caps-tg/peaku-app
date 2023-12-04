b05a98cd226c2b6604c6d2bc7dc057b45be6d4b4
provo-caps-tg <provocapstg@gmail.com>
2023-12-04 11:54:50 -0700

    order code

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:flutter/services.dart' show rootBundle;
 void main() {runApp(const PeakUApp());}
 String pass = '';
 String email = '';
+bool enabled = true;
 bool responded = false;
 String userResponse = '';
-bool enabled = true;
 String selectedQuestion = 'Loading...';
 Color blue = const Color.fromARGB(255, 15, 49, 86);
 Color orange = const Color.fromARGB(255, 232, 105, 0);
