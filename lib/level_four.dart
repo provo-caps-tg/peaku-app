86f5b9723582d8572e19ebe6c6b3d8dfdf5b5718
provo-caps-tg <provocapstg@gmail.com>
2024-02-02 11:22:18 -0700

    Structure and archive

--- lib/level_four.dart
+++ lib/level_four.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/material.dart';
+import 'global_varibles.dart';
+import 'main.dart';
+import 'survey_intro.dart';
+import 'level_one.dart'; 
+import 'level_two.dart';
+import 'level_three.dart';
--- lib/level_one.dart
+++ lib/level_one.dart
@@ -1,1 +1,21 @@
 import 'package:flutter/material.dart';
+import 'global_varibles.dart';
+import 'main.dart';
+import 'survey_intro.dart';
+import 'level_two.dart';
+import 'level_three.dart';
+import 'level_four.dart';
+
+class LevelOneHome extends StatefulWidget {
+  const LevelOneHome({super.key});
+  @override
+  State<LevelOneHome> createState() => _LevelOneHomeState();}
+
+class _LevelOneHomeState extends State<LevelOneHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
