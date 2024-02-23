bac143977f1f543d28c92674e102b4d7c59d1424
provo-caps-tg <provocapstg@gmail.com>
2024-02-23 10:33:10 -0700

    import

--- lib/level_one.dart
+++ lib/level_one.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:survey_kit/survey_kit.dart';
+import 'global_varibles.dart';
 
 class LevelOneHome extends StatefulWidget {
   const LevelOneHome({super.key});
@@ -9,8 +10,54 @@ class LevelOneHome extends StatefulWidget {
 class _LevelOneHomeState extends State<LevelOneHome> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: orange, foregroundColor: Colors.white, toolbarHeight: 3,
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            constraints: const BoxConstraints(maxWidth: 500),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(Radius.circular(29))
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: Column(
+                        //Start of widgets//
+                        children: [
+                          
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
