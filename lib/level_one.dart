b6b0dba9e5e32e853fd7047e6c5c8bc502e62bca
provo-caps-tg <provocapstg@gmail.com>
2024-03-04 12:43:59 -0700

    RAHHH!!!!!

--- lib/level_one.dart
+++ lib/level_one.dart
@@ -1,4 +1,5 @@
 import 'global_varibles.dart';
+import 'survey_intro.dart';
 import 'package:flutter/material.dart' hide Step;
 import 'package:survey_kit/survey_kit.dart';
 
@@ -12,13 +13,258 @@ class LevelOneHome extends StatefulWidget {
 
 class _LevelOneHomeState extends State<LevelOneHome> {
   var stepss = [
-QuestionStep(
-    title: 'Sample title',
-    text: 'Sample text',
-    answerFormat: const TextAnswerFormat(
-        maxLines: 5,
+    QuestionStep(
+      title: 'What Makes for a Healthy Relationship?',
+      text: 'Select the top 5 qualities from the group below that make for healthy relationships:',
+      isOptional: false,
+      answerFormat: const MultipleChoiceAnswerFormat(
+        maxAnswers: 5,
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Respect', value: 'Respect'),
+          TextChoice(text: 'Love', value: 'Love'),
+          TextChoice(text: 'Kindness', value: 'Kindness'),
+          TextChoice(text: 'Contentment', value: 'Contentment'),
+          TextChoice(text: 'Understanding the other person', value: 'Understanding the other person'),
+          TextChoice(text: 'Generosity', value: 'Generosity'),
+          TextChoice(text: 'Care', value: 'Care'),
+          TextChoice(text: 'Empathy', value: 'Empathy'),
+          TextChoice(text: 'Mutual effort', value: 'Mutual effort'),
+          TextChoice(text: 'Loyalty', value: 'Loyalty'),
+          TextChoice(text: 'Humor', value: 'Humor'),
+          TextChoice(text: 'Playful Teasing', value: 'Playful Teasing'),
+          TextChoice(text: 'Righteousness', value: 'Righteousness'),
+          TextChoice(text: 'Being in the Lead Role', value: 'Being in the Lead Role'),
+          TextChoice(text: 'Intelligence', value: 'Intelligence'),
+          TextChoice(text: 'Acceptance', value: 'Acceptance'),
+          TextChoice(text: 'Educating Them When They are Wrong', value: 'Educating Them When They are Wrong'),
+          TextChoice(text: 'Loyalty', value: 'Loyalty'),
+          TextChoice(text: 'Sharing Values', value: 'Sharing Values'),
+          TextChoice(text: 'Spontaneity', value: 'Spontaneity'),
+        ],
+      ),
     ),
-)
+    QuestionStep(
+      title: 'Are the qualities you selected present in your current relationships?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'Somewhat', value: 'Somewhat'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Are the qualities you selected present in your work relationships?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'Somewhat', value: 'Somewhat'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Are the qualities you selected present in your family relationships?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'Somewhat', value: 'Somewhat'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Are the qualities you selected present in your friend relationships?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'Somewhat', value: 'Somewhat'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Are the qualities you selected present in your intimate or romantic relationship?',
+      isOptional: false,
+      buttonText: "Let's go a little deeper.",
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'Somewhat', value: 'Somewhat'),
+          TextChoice(text: 'No', value: 'No'),
+          TextChoice(text: 'Not applicable', value: 'Not applicable'),
+        ],
+      ),
+    ),
+    InstructionStep(
+      title: 'What does respect look like to you?',
+      text: '(Choose from the following examples whether they are YES Respectful or NO Not Respectful)',
+      buttonText: 'Let\'s go!',
+    ),
+    QuestionStep(
+      title: 'My partner makes my lunch every day. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'My boss gave me an advance even though I did not ask for one. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'My partner put a tracker app on my phone. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: "I'm not allowed to lock my bedroom door. Respectful?",
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: "I don't always tell the truth to those close to me. Respectful?",
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Sometimes I speak rudely to those close to me. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'If I make a mess in my relationship, I always clean it up. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'I take responsibility for my behavior. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'I expect others to know what I need without me having to say. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'My partner sometimes says no, but I keep pushing anyway. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'I thank my partner / friends for what they do for me. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'I acknowledge my partner for the great qualities they have. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'If my partner doesn’t look good today, I will tell them. Respectful?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Would you and another person agree on what respect is?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Would everyone define LOVE in the same way?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
+    QuestionStep(
+      title: 'Would everyone agree with you on what respect looks like?',
+      isOptional: false,
+      answerFormat: const SingleChoiceAnswerFormat(
+        textChoices: <TextChoice>[
+          TextChoice(text: 'Yes', value: 'Yes'),
+          TextChoice(text: 'No', value: 'No'),
+        ],
+      ),
+    ),
   ];
 
   @override
@@ -77,6 +323,27 @@ QuestionStep(
                           ),
                         ),
                         ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, top: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: orange,
+                      borderRadius: BorderRadius.circular(13),
+                    ),
+                    child: IconTheme(
+                      data: const IconThemeData(
+                      color: Colors.white),
+                      child: IconButton(
+                        onPressed:() {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => const IntroPage()), );
+                            }, 
+                        icon: const Icon(Icons.arrow_back)
                       ),
                     ),
                   ),
